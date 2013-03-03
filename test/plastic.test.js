350cb65be8358da8e2106c3ede9cc1f3a8c4a744
Ethan Resnick <ethan.resnick@gmail.com>
2013-03-03 08:14:49 -0500

    Skeleton test file

---
+++ test/plastic.test.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var plastic = require('../index.js');
+
+suite('plastic', function() {
+
+  console.log('\n\n\033[34mTesting Plastic\033[0m');
+
+  //
+  test('', function (done) {
+    done();
+  });
+});
